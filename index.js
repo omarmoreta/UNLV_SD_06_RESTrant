d6aae4a05d80de2fc0e28377a0d336c273356326
Omar <o.moreta@gmail.com>
2023-02-10 02:59:58 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+const placesController = require("./controllers/places_controller");
+
+app.use("/places", placesController);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
