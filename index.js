ceccdd6dd474435c775c62283b38de91c1a54cdb
Omar <o.moreta@gmail.com>
2023-02-13 05:27:20 -0500

    adding error404 page view

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.status(404).send("Error404");
 });
 
 app.listen(PORT, () => {
